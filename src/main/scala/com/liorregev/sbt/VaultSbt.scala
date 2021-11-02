c5da20d0de44cf3c85dd798d80e3db3d30b7b278
Lior Regev <lior.regev@placer.ai>
2021-11-02 11:12:37 +0200

    Added vaultAddress and loginMethod keys

--- src/main/scala/com/liorregev/sbt/VaultSbt.scala
+++ src/main/scala/com/liorregev/sbt/VaultSbt.scala
@@ -1,27 +1,65 @@
 package com.liorregev.sbt
 
-import sbt._
-import Keys._
+import com.bettercloud.vault.api.Auth
 import com.bettercloud.vault.{Vault, VaultConfig}
+import sbt.Keys._
+import sbt._
 
 import scala.collection.JavaConverters._
 
-object VaultSbt extends AutoPlugin {
-  object autoImport {
+object VaultSbt {
+  sealed trait LoginMethod
+  object LoginMethods {
+    case object None extends LoginMethod
+    final case class UserPass(user: String, password: String) extends LoginMethod
+  }
+  object VaultKeys {
+    val vaultAddress = settingKey[String]("Address of the Vault server")
+    val loginMethod = settingKey[LoginMethod]("Method to log in with")
     val resolveCreds = taskKey[Unit]("say hello")
   }
 
-  import autoImport._
-  override lazy val projectSettings: Seq[Setting[_]] = Seq(
+  object vault {
+    val vaultAddress = VaultKeys.vaultAddress
+    val resolveCredentials = VaultKeys.resolveCreds
+    val loginMethod = VaultKeys.loginMethod
+    val loginMethods: LoginMethods.type = LoginMethods
+  }
+
+  import VaultKeys._
+
+  def loadToken(config: VaultConfig): LoginMethod => String = {
+    case LoginMethods.None => config.getToken
+    case LoginMethods.UserPass(user, password) =>
+      new Auth(config).loginByUserPass(user, password).getAuthClientToken
+  }
+
+  def projectSettings: Seq[Setting[_]] = Seq(
+    vaultAddress := "",
+    loginMethod := LoginMethods.None,
     resolveCreds := {
       val s = streams.value
       val config = new VaultConfig()
-        .address("http://127.0.0.1:8200")
-        .token("s.u5nlha7girpxOiQbnxmGEDrJ")
-        .build()
-      val vault = new Vault(config)
-      val secret = vault.logical().read("secret/services/jfrog").getData.asScala
-      s.log.info(secret.mkString(","))
+        .address(vaultAddress.value)
+      val token = loadToken(config.build())(loginMethod.value)
+      val vaultClient = new Vault(config.token(token).build())
+      val response = vaultClient.logical().read("secret/services/jfrog")
+      if(Option(response.getLeaseId).isEmpty) {
+        s.log.warn("Could not resolve credentials from Vault")
+      } else {
+        val secret = response.getData.asScala
+        s.log.info(secret.mkString(","))
+      }
     }
   )
+}
+
+object VaultPlugin extends AutoPlugin {
+  override def requires: Plugins = sbt.plugins.CorePlugin
+
+  object autoImport {
+    val vault: VaultSbt.vault.type = VaultSbt.vault
+  }
+
+  override lazy val projectSettings: Seq[Setting[_]] = VaultSbt.projectSettings
 }
