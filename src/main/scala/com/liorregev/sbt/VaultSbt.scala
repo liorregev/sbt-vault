6321843371b205186d7cabf581673a4d5f401183
Lior Regev <lior.regev@placer.ai>
2022-01-12 11:24:36 +0100

    Added Token login type

--- src/main/scala/com/liorregev/sbt/VaultSbt.scala
+++ src/main/scala/com/liorregev/sbt/VaultSbt.scala
@@ -17,18 +17,25 @@ object VaultSbt {
     case object None extends LoginMethod
     final case class UserPass(user: String, password: String) extends LoginMethod
     final case class GCPServiceAccount(role: String) extends LoginMethod
+    final case class Token(token: String) extends LoginMethod
   }
+
+
   object VaultKeys {
     val vaultAddress = settingKey[String]("Address of the Vault server")
+    val credentialsKeys = settingKey[Seq[vault.CredentialsKey]]("Data on credentials to fetch")
     val loginMethod = settingKey[LoginMethod]("Method to log in with")
-    val resolveCreds = taskKey[Unit]("say hello")
+    val resolveCreds = taskKey[Either[List[String], List[Credentials]]]("Resolve credentials")
   }
 
   object vault {
     val vaultAddress = VaultKeys.vaultAddress
+    val credentialsKeys = VaultKeys.credentialsKeys
     val resolveCredentials = VaultKeys.resolveCreds
     val loginMethod = VaultKeys.loginMethod
     val loginMethods: LoginMethods.type = LoginMethods
+    final case class CredentialsKey(vaultKey: String, internalUserKey: String, internalPasswordKey: String,
+                                    realm: String, host: String)
   }
 
   import VaultKeys._
@@ -69,26 +76,50 @@ object VaultSbt {
     case LoginMethods.None => config.getToken
     case LoginMethods.UserPass(user, password) =>
       new Auth(config).loginByUserPass(user, password).getAuthClientToken
-    case LoginMethods.GCPServiceAccount(role) =>
-      loadTokenFromGCP(role, config)
+    case LoginMethods.GCPServiceAccount(role) => loadTokenFromGCP(role, config)
+    case LoginMethods.Token(token) => token
   }
 
   def projectSettings: Seq[Setting[_]] = Seq(
     vaultAddress := "",
+    credentialsKeys := Seq.empty,
     loginMethod := LoginMethods.None,
     resolveCreds := {
-      val s = streams.value
       val config = new VaultConfig()
         .address(vaultAddress.value)
       val token = loadToken(config.build())(loginMethod.value)
       val vaultClient = new Vault(config.token(token).build())
-      val response = vaultClient.logical().read("kv/shared_access/consul")
-      if(Option(response.getRestResponse).exists(_.getStatus == 200)) {
-        val secret = response.getData.asScala
-        s.log.info(secret.mkString(","))
-      } else {
-        s.log.warn("Could not resolve credentials from Vault")
-      }
+      credentialsKeys.value
+        .map(key => {
+          val response = vaultClient.logical().read(key.vaultKey)
+          Option(response.getRestResponse)
+            .map(resp => {
+              if(resp.getStatus == 200) {
+                val secret = response.getData.asScala
+                Right(Credentials(key.realm, key.host, secret(key.internalUserKey), secret(key.internalPasswordKey)))
+              } else {
+                Left(s"Could not resolve credentials from Vault (${key.vaultKey}): ${resp.getStatus}")
+              }
+            })
+            .getOrElse(Left(s"Could not resolve credentials from Vault (${key.vaultKey})"))
+        })
+        .foldLeft[Either[List[String], List[Credentials]]](Right(Nil)) {
+          case (Left(errorsSoFar), Left(error)) => Left(error :: errorsSoFar)
+          case (Left(errorsSoFar), _) => Left(errorsSoFar)
+          case (Right(credsSoFar), Right(creds)) => Right(creds :: credsSoFar)
+          case (Right(_), Left(error)) => Left(List(error))
+        }
+    },
+    ThisBuild / credentials ++= {
+      val logger = streams.value.log
+      val resolvedCredentials = resolveCreds.value
+      resolvedCredentials.fold(
+        msgs => {
+          msgs.foreach(msg => logger.error(msg))
+          Seq.empty
+        },
+        identity
+      )
     }
   )
 }
