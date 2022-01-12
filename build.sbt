2583f876bc886874fd9b33e05dc084e710bcdf11
Lior Regev <lior.regev@placer.ai>
2022-01-12 09:38:39 +0100

    Adding GCP Login type Workaround for jitpack

--- build.sbt
+++ build.sbt
@@ -1,5 +1,21 @@
+import sbt.io.Path.userHome
+
 ThisBuild / organization := "com.github.liorregev"
 ThisBuild / homepage := Some(url("https://github.com/sbt/sbt-hello"))
+ThisBuild / resolvers += "jitpack" at "https://jitpack.io"
+ThisBuild / licenses := Seq(
+  ("Apache-2.0", url("http://www.apache.org/licenses/LICENSE-2.0"))
+)
+
+val debugMode = false
+
+lazy val LocalMavenResolverForSbtPlugins = {
+  // remove scala and sbt versions from the path, as it does not work with jitpack
+  val pattern  = "[organisation]/[module]/[revision]/[module]-[revision](-[classifier]).[ext]"
+  val name     = "local-maven-for-sbt-plugins"
+  val location = userHome / ".m2" / "repository"
+  Resolver.file(name, location)(Patterns().withArtifactPatterns(Vector(pattern)))
+}
 
 lazy val root = (project in file("."))
   .enablePlugins(SbtPlugin)
@@ -12,10 +28,17 @@ lazy val root = (project in file("."))
       }
     },
     libraryDependencies ++= Seq(
-      "com.bettercloud" % "vault-java-driver" % "5.1.0"
+      "com.google.cloud"               % "google-cloud-iamcredentials" % "2.0.5",
+      "com.softwaremill.sttp.client3" %% "core"                        % "3.3.16",
+      "com.typesafe.play"             %% "play-json"                   % "2.9.2",
+      "com.bettercloud"                % "vault-java-driver"           % "5.1.0"
     ),
     scriptedLaunchOpts := { scriptedLaunchOpts.value ++
-      Seq("-Xmx1024M", "-Dplugin.version=" + version.value)
+      Seq("-Xmx1024M", "-Dplugin.version=" + version.value) ++
+      (if(debugMode) Seq("-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=5005") else Nil)
     },
-    scriptedBufferLog := false
+    scriptedBufferLog := false,
+    publishMavenStyle := true,
+    resolvers += LocalMavenResolverForSbtPlugins,
+    publishM2Configuration := publishM2Configuration.value.withResolverName(LocalMavenResolverForSbtPlugins.name)
   )
--- src/main/scala/com/liorregev/sbt/VaultSbt.scala
+++ src/main/scala/com/liorregev/sbt/VaultSbt.scala
@@ -2,9 +2,13 @@ package com.liorregev.sbt
 
 import com.bettercloud.vault.api.Auth
 import com.bettercloud.vault.{Vault, VaultConfig}
+import com.google.cloud.iam.credentials.v1.{IamCredentialsClient, IamCredentialsSettings, SignJwtRequest}
+import play.api.libs.json.{JsNumber, JsObject, JsString, JsValue}
 import sbt.Keys._
 import sbt._
+import sttp.client3._
 
+import java.time.Instant
 import scala.collection.JavaConverters._
 
 object VaultSbt {
@@ -12,6 +16,7 @@ object VaultSbt {
   object LoginMethods {
     case object None extends LoginMethod
     final case class UserPass(user: String, password: String) extends LoginMethod
+    final case class GCPServiceAccount(role: String) extends LoginMethod
   }
   object VaultKeys {
     val vaultAddress = settingKey[String]("Address of the Vault server")
@@ -28,10 +33,44 @@ object VaultSbt {
 
   import VaultKeys._
 
+  def loadTokenFromGCP(role: String, config: VaultConfig): String = {
+    val backend = HttpURLConnectionBackend()
+    val saEmail = basicRequest
+      .get(uri"http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/email")
+      .header("Metadata-Flavor", Option("Google"))
+      .send(backend)
+      .body
+      .fold(identity, identity)
+
+    val iamClient = {
+      val iamCredentialsSettings = IamCredentialsSettings.newBuilder().build()
+      IamCredentialsClient.create(iamCredentialsSettings)
+    }
+    val now = Instant.now()
+    val expires = now.plusSeconds(900)
+    val payload = JsObject(
+      Map[String, JsValue](
+        "iat" -> JsNumber(now.getEpochSecond),
+        "exp" -> JsNumber(expires.getEpochSecond),
+        "sub" -> JsString(saEmail),
+        "aud" -> JsString(s"vault/$role")
+      )
+    )
+    val saName = s"projects/-/serviceAccounts/$saEmail"
+    val request = SignJwtRequest.newBuilder()
+      .setName(saName)
+      .setPayload(payload.toString())
+      .build()
+    val jwt = iamClient.signJwt(request).getSignedJwt
+    new Auth(config).loginByGCP(role, jwt).getAuthClientToken
+  }
+
   def loadToken(config: VaultConfig): LoginMethod => String = {
     case LoginMethods.None => config.getToken
     case LoginMethods.UserPass(user, password) =>
       new Auth(config).loginByUserPass(user, password).getAuthClientToken
+    case LoginMethods.GCPServiceAccount(role) =>
+      loadTokenFromGCP(role, config)
   }
 
   def projectSettings: Seq[Setting[_]] = Seq(
@@ -43,12 +82,12 @@ object VaultSbt {
         .address(vaultAddress.value)
       val token = loadToken(config.build())(loginMethod.value)
       val vaultClient = new Vault(config.token(token).build())
-      val response = vaultClient.logical().read("secret/services/jfrog")
-      if(Option(response.getLeaseId).isEmpty) {
-        s.log.warn("Could not resolve credentials from Vault")
-      } else {
+      val response = vaultClient.logical().read("kv/shared_access/consul")
+      if(Option(response.getRestResponse).exists(_.getStatus == 200)) {
         val secret = response.getData.asScala
         s.log.info(secret.mkString(","))
+      } else {
+        s.log.warn("Could not resolve credentials from Vault")
       }
     }
   )
